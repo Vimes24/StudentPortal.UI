c0ec92a6a0c3cc2379bfc5ad1a9fed5ca5ae18fe
Wojciech <skrzypek.wojciech@gmail.com>
2022-06-09 18:32:29 +0200

    Zaimplementowano UI, w tym wyświetlanie tabeli z danymi studentów, filtrowanie, sortowanie i paginację.

---
+++ studentportal-ui/src/app/models/ui-models/student.model.ts
@@ -0,0 +1,15 @@
+import { Address } from "./address.model";
+import { Gender } from "./gender.model";
+
+export interface Student {
+  id: string,
+  firstName: string,
+  lastName: string,
+  dateofBirth: string,
+  email: string,
+  phoneNumber: string,
+  profileImageUri: string,
+  genderId: string,
+  gender: Gender,
+  address: Address
+}
