7c4c84f250ee99944110f71a61afde324a58fd41
Wojciech <skrzypek.wojciech@gmail.com>
2022-06-27 12:08:13 +0200

    Dodano obsługę tworzenia nowego studenta.

--- studentportal-ui/src/app/students/view-student/view-student.component.ts
+++ studentportal-ui/src/app/students/view-student/view-student.component.ts
@@ -31,7 +31,10 @@ export class ViewStudentComponent implements OnInit {
       physicalAddress: '',
       postalAddress: ''
     }
-  }
+  };
+
+  isNewStudent = true;
+  header = '';
 
   genderList: Gender[] = [];
 
@@ -46,10 +49,19 @@ export class ViewStudentComponent implements OnInit {
       this.studentId = params.get('id');
 
       if (this.studentId) {
-        this.studentService.getStudent(this.studentId)
-          .subscribe((successResponse) => {
-            this.student = successResponse;
-          });
+
+        if (this.studentId.toLowerCase() == 'Add'.toLowerCase()) {
+          this.isNewStudent = true;
+          this.header = 'Add New Student';
+        }
+        else {
+          this.isNewStudent = false;
+          this.header = 'Edit Student';
+          this.studentService.getStudent(this.studentId)
+            .subscribe((successResponse) => {
+              this.student = successResponse;
+            });
+        }
 
         this.genderService.getGenderList()
           .subscribe(
@@ -85,6 +97,24 @@ export class ViewStudentComponent implements OnInit {
             this.router.navigateByUrl('students');
           }, 2000);
           
+        },
+        (errorResponse) => {
+          console.log(errorResponse);
+        });
+  }
+
+  onAdd(): void {
+    this.studentService.addStudent(this.student)
+      .subscribe(
+        (successResponse) => {
+          this.snackbar.open('Student added successfully', undefined, {
+            duration: 2000
+          });
+
+          setTimeout(() => {
+            this.router.navigateByUrl(`students/${successResponse.id}`);
+          }, 2000);
+
         },
         (errorResponse) => {
           console.log(errorResponse);
